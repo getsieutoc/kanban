3dd62e38e96aa652ea500e5744b6402bac40b1ee
Sang Dang <sang@dang.to>
2025-04-24 00:07:58 +0300

    clean up auth

--- auth.ts
+++ auth.ts
@@ -1,10 +1,7 @@
 import { prismaAdapter } from 'better-auth/adapters/prisma';
-import { createAuthMiddleware } from 'better-auth/api';
 import { nextCookies } from 'better-auth/next-js';
 import { magicLink } from 'better-auth/plugins';
-import { expo } from '@better-auth/expo';
 import { betterAuth } from 'better-auth';
-import { isAuthPath } from '@/lib/utils';
 import { sendEmail } from '@/lib/email';
 import { prisma } from '@/lib/prisma-client';
 import { headers } from 'next/headers';
@@ -19,8 +16,6 @@ export const auth = betterAuth({
   },
   emailVerification: {
     sendVerificationEmail: async ({ user, url, token }, request) => {
-      console.log('Signed Up', { request, url, token });
-
       await sendEmail({
         to: user.email,
         subject: 'Verify Kanban Account',
@@ -28,56 +23,9 @@ export const auth = betterAuth({
       });
     },
   },
-  socialProviders: {
-    google: {
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    },
-  },
-  trustedOrigins: ['sieutoc-Kanban://'],
-  user: {
-    additionalFields: {
-      activeProfileId: {
-        type: 'string',
-        required: false,
-        defaultValue: null,
-        input: false,
-        fieldName: 'activeProfileId',
-      },
-    },
-  },
-  hooks: {
-    after: createAuthMiddleware(async (ctx) => {
-      // DEBUG:
-      // console.log({ path: ctx.path, newSession: ctx.context.newSession });
-      // Only execute this at early stage of registration
-      if (isAuthPath(ctx.path) && ctx.context.newSession) {
-        const { user } = ctx.context.newSession;
-
-        if (user && !user.activeProfileId) {
-          const newProfile = await prisma.profile.create({
-            data: {
-              name: 'Default',
-              userId: user.id,
-            },
-          });
-
-          await prisma.user.update({
-            where: { id: user.id },
-            data: { activeProfileId: newProfile.id },
-          });
-        }
-      }
-    }),
-  },
   plugins: [
-    expo(),
     magicLink({
       sendMagicLink: async ({ email, url }) => {
-        // console.log('Magic', { request, url, token });
-        // const expourl = `sieutoc-Kanban://new/api/auth/magic-link/verify?token=${token}`;
-        // console.log('expourl', expourl);
-
         await sendEmail({
           to: email,
           subject: 'Login to Kanban',
@@ -114,18 +62,7 @@ export const getAuth = async () => {
     };
   }
 
-  const { user } = data;
-
-  if (!user.activeProfileId) {
-    throw new Error('Can not get active profile ID');
-  }
-
-  const profile = await prisma.profile.findUnique({
-    where: { id: user.activeProfileId },
-  });
-
   return {
     ...data,
-    profile,
   };
 };
